da9b9f2f255a238b80ed025d67cfd8b5e31e60f1
Chintan Joshi <cjjoshi@ucsd.edu>
2017-01-05 11:34:35 -0800

    removed these files, sharing them again

--- find_duplicate_metid.m
+++
@@ -1,9 +0,0 @@
-function [dup_id,dup_name] = find_duplicate_metid(metid,name)
-% [dup_id,dup_name] = find_duplicate_metid(metid,name)
-% identifies duplicate metabolite ids
-
-[~,i] = unique(metid);
-i = setdiff([1:1:length(metid)],i);
-dup_id = metid(i);
-dup_name = name(i);
-
