898b1204aefdabd6c37d98ec41c4e6f900da9c9e
Chintan Joshi <cjjoshi@ucsd.edu>
2017-01-14 16:18:22 -0800

    Functions for charge balancing

---
+++ charge_balance.m
@@ -0,0 +1,28 @@
+function [charge_imb,charge] = charge_balance(model,rxns,disp_flag)
+
+if nargin < 2
+    rxns = model.rxns;
+end
+if nargin < 3
+    disp_flag = false;
+end
+charge_imb = false;
+charge = zeros(length(rxns),1);
+for i=1:length(rxns)
+    charge(i) = sum(full(model.S(:,i)'*double(model.metCharge)));
+    if charge(i)==0
+        charge_imb(i,1) = true;
+    else
+        charge_imb(i,1) = false;
+    end
+end
+if disp_flag
+    if sum(charge_imb)==size(model.rxns,1)
+        fprintf('Reaction(s) is(are) charge balanced.');
+    else
+        fprintf('%d reaction(s) not balanced.\nReaction id\t\tNet charge\n---------------------------\n',sum(charge_imb))
+        for i=1:length(rxns)
+            fprintf('%s\t\t%d\n',rxns{i,1},charge(i));
+        end
+    end
+end
